58ca0fdc1c3fa502b057432cfd926911c8f1952a
Houssem Eddine Chibouni <109615722+softsurgery@users.noreply.github.com>
2024-11-11 21:40:55 +0100

    instruction 5

---
+++ src/Zoo.java
@@ -0,0 +1,6 @@
+public class Zoo {
+    Animal[] animals = new Animal[25];
+    String name;
+    String city;
+    int nbrCages;
+}
--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -2,28 +2,17 @@ import java.util.Scanner;
 
 public class ZooManagement {
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        int nbrCages;
+        Animal lion = new Animal();
+        Zoo myZoo = new Zoo();
 
-        do {
-            System.out.print("Enter the number of cages in the zoo: ");
-            nbrCages = scanner.nextInt();
-            scanner.nextLine();
-        }while(nbrCages<0);
+        lion.age = 5;
+        lion.family = "Feline";
+        lion.isMammal = true;
+        lion.name = "Leo";
 
-        String zooName;
-        do {
-            System.out.print("Enter the name of the zoo: ");
-            zooName = scanner.nextLine();
-        }while(zooName.length()<3);
-
-        if (nbrCages == 0) {
-            System.out.println(zooName+" ne comporte pas des cages");
-        } else if (nbrCages == 1) {
-            System.out.println(zooName+" comporte un seul cage");
-        } else {
-            System.out.println(zooName+" comporte "+nbrCages+" cages");
-        }
-
+        myZoo.name = "Hello Zoo";
+        myZoo.city = "New York";
+        myZoo.nbrCages = 10;
+        myZoo.animals[0] = lion;
     }
 }
