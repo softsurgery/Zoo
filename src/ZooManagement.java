13d60f7842a1db559ce2d67919d38f290b20dbc1
Houssem Eddine Chibouni <109615722+softsurgery@users.noreply.github.com>
2024-11-11 21:58:18 +0100

    instruction 7

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -1,6 +1,8 @@
 public class ZooManagement {
     public static void main(String[] args){
         Animal lion = new Animal("Feline","Leo",5,true);
+        Animal tigre = new Animal("Feline","Xao",2,true);
+        Animal requin = new Animal("Poisson","ShiShi",3,false);
         Zoo myZoo = new Zoo("Hello Zoo","New York",10);
     }
 }
