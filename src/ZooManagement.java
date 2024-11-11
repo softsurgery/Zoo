7eec1a34284d34b4e33b6e92652ee29c0fa231b4
Houssem Eddine Chibouni <109615722+softsurgery@users.noreply.github.com>
2024-11-11 21:19:41 +0100

    Initial commit

---
+++ src/ZooManagement.java
@@ -0,0 +1,7 @@
+public class ZooManagement {
+    public static void main(String[] args){
+        int nbrCages = 20;
+        String zooName = "my zoo";
+        System.out.println(zooName+" comporte " + nbrCages + " cages");
+    }
+}
