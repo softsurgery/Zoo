98c344ced571ae699db2877a853c2e7c4bf3db57
Houssem Eddine Chibouni <109615722+softsurgery@users.noreply.github.com>
2024-11-11 21:29:15 +0100

    instruction 2

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+
 public class ZooManagement {
     public static void main(String[] args){
-        int nbrCages = 20;
-        String zooName = "my zoo";
-        System.out.println(zooName+" comporte " + nbrCages + " cages");
+        Scanner scanner = new Scanner(System.in);
+        int nbrCages;
+
+        do {
+            System.out.print("Enter the number of cages in the zoo: ");
+            nbrCages = scanner.nextInt();
+            scanner.nextLine();
+        }while(nbrCages<0);
+
+        String zooName;
+        do {
+            System.out.print("Enter the name of the zoo: ");
+            zooName = scanner.nextLine();
+        }while(zooName.length()<3);
+
+        if (nbrCages == 0) {
+            System.out.println(zooName+" ne comporte pas des cages");
+        } else if (nbrCages == 1) {
+            System.out.println(zooName+" comporte un seul cage");
+        } else {
+            System.out.println(zooName+" comporte "+nbrCages+" cages");
+        }
+
     }
 }
